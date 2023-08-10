03146f8e59757d2a1b743e5d5e69ed22ac830185
dlskawo0409 <dlskawo0409@gmail.com>
2023-08-10 22:51:07 +0900

    init

---
+++ spring/petAnomaly/src/main/java/com/example/petAnomaly/PetAnomalyApplication.java
@@ -0,0 +1,13 @@
+package com.example.petAnomaly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetAnomalyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetAnomalyApplication.class, args);
+	}
+
+}
---
+++ spring/petAnomaly/src/test/java/com/example/petAnomaly/PetAnomalyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petAnomaly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetAnomalyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
